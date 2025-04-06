6a0a79b26974baa1613d31c274ff2f60f90b99ab
Greg <bgregmc@icloud.com>
2025-04-05 20:04:08 -0700

    attach previews to user records, and validate urls

--- app/models/preview.rb
+++ app/models/preview.rb
@@ -1,3 +1,5 @@
 class Preview < ApplicationRecord
-  # TODO: url validation
+  validates :url, presence: true, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
+
+  belongs_to :user, optional: true
 end
---
+++ db/migrate/20250405235349_add_user_ref_to_previews.rb
@@ -0,0 +1,5 @@
+class AddUserRefToPreviews < ActiveRecord::Migration[8.0]
+  def change
+    add_reference :previews, :user, null: false, foreign_key: true
+  end
+end
